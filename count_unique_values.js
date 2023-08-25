02b88819c927a4bfa6af969142588f5da095ba1e
maen <maenkh@yahoo.com>
2023-08-25 10:36:20 +0300

    refactor

--- count_unique_values.js
+++ count_unique_values.js
@@ -1,11 +1,24 @@
 function countUniqueValue(arr){
-    let obj = {}
+    // let obj = {}
 
-    arr.forEach(element => {
-        (obj[element]) ? obj[element]+=1 : obj[element]=1;
-    });
+    // arr.forEach(element => {
+    //     (obj[element]) ? obj[element]+=1 : obj[element]=1;
+    // });
 
-    return Object.keys(obj).length
+    // return Object.keys(obj).length
+
+    let i=0,j=1;
+
+    while(j < arr.length){
+        if(arr[i] == arr[j]){
+            ++j;          
+        }else{
+            ++i;
+            arr[i]= arr[j];
+            ++j;
+        }
+    }
+    return ++i;
 }
 
-console.log(countUniqueValue([1,1,1,2,3,4,2,2,2,5,4]))
+console.log(countUniqueValue([1,1,1,2,3,4,4,5,5,5,6,6,6,6,6,6,7]))
