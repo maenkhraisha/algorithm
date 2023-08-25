93d3c537e5288445d8cc9063a93e66ca43ed0d16
maen <maenkh@yahoo.com>
2023-08-25 09:24:48 +0300

    refactor

--- same_square.js
+++ same_square.js
@@ -5,6 +5,8 @@ function sameSquare(arr, sqrArr) {
   // if both array lenghth is zero then return true
   // else return false
 
+/*
+// naive solution 
   if (arr.length != sqrArr.length) return false;
 
   for (let i = 0; i < arr.length; i++) {
@@ -15,10 +17,38 @@ function sameSquare(arr, sqrArr) {
     sqrArr.splice(indexofsqr, 1);
   }
   return true;
+*/
+
+// refactore solution
+let freqarr = {}
+let freqsqr = {}
+
+arr.forEach(element => {
+    freqarr[element] = (freqarr[element] || 0) + 1
+});
+
+sqrArr.forEach(element => {
+    freqsqr[element] = (freqsqr[element] || 0) + 1
+})
+
+console.log(freqarr);
+console.log(freqsqr);
+
+for(const key in freqarr){
+    if(!((key ** 2) in freqsqr)){
+        return false
+    }
+    if(!(freqarr[key] == freqsqr[key**2])){
+        return false
+    }
 }
 
+return true
+
+}
+
+
+
 // test the function
-let t1 = performance.now();
-console.log(sameSquare([1, 1, 2, 3, 1], [1, 4, 9, 1, 1]));
-let t2 = performance.now();
-console.log(t2 - t1);
+console.log(sameSquare([1, 1, 2, 3, 1, 3], [1, 4, 9, 1, 1,9]));
+
