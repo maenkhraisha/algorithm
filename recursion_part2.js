3c6dd3cff51d890cd4f519f92ce151cd14f82943
maen <maenkh@yahoo.com>
2023-08-26 22:17:19 +0300

    linear search

--- recursion_part2.js
+++ recursion_part2.js
@@ -58,13 +58,15 @@ function isPalindrome(str){
 // console.log(isPalindrome('amanaplanacanalpanama')) // true
 // console.log(isPalindrome('amanaplanacanalpandemonium')) // false
 
+
+
 function someRecursive(arr,callback){
     // add whatever parameters you deem necessary - good luck!
     
-    
+
     
   }
-console.log(someRecursive([1,2,3,4], isOdd)); // true
-console.log(someRecursive([4,6,8,9], isOdd)); // true
-console.log(someRecursive([4,6,8], isOdd));// false
-console.log(someRecursive([4,6,8], val => val > 10));// false
+// console.log(someRecursive([1,2,3,4], isOdd)); // true
+// console.log(someRecursive([4,6,8,9], isOdd)); // true
+// console.log(someRecursive([4,6,8], isOdd));// false
+// console.log(someRecursive([4,6,8], val => val > 10));// false
