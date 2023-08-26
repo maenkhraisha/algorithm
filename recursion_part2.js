04663f4c8b8c053082300e800069f0c0cd0fdbfc
maen <maenkh@yahoo.com>
2023-08-26 21:37:50 +0300

    recursion 2

--- recursion_part2.js
+++ recursion_part2.js
@@ -31,5 +31,40 @@ function reverse(str){
     return reverseStr;
 }
   
- console.log(reverse('awesome')) // 'emosewa'
+//  console.log(reverse('awesome')) // 'emosewa'
 //  console.log(reverse('rithmschool')) // 'loohcsmhtir'
+
+
+function isPalindrome(str){
+    // add whatever parameters you deem necessary - good luck!
+
+    let revStr = '';
+    function reverse(helperStr){
+        if(helperStr.length == 0) return ;
+
+        revStr =  helperStr[0] + revStr;
+
+        return reverse(helperStr.slice(1));
+    }
+
+    reverse(str);
+    return  (str === revStr)
+
+  }
+
+// console.log(isPalindrome('awesome')) // false
+// console.log(isPalindrome('foobar')) // false
+// console.log(isPalindrome('tacocat')) // true
+// console.log(isPalindrome('amanaplanacanalpanama')) // true
+// console.log(isPalindrome('amanaplanacanalpandemonium')) // false
+
+function someRecursive(arr,callback){
+    // add whatever parameters you deem necessary - good luck!
+    
+    
+    
+  }
+console.log(someRecursive([1,2,3,4], isOdd)); // true
+console.log(someRecursive([4,6,8,9], isOdd)); // true
+console.log(someRecursive([4,6,8], isOdd));// false
+console.log(someRecursive([4,6,8], val => val > 10));// false
