3b63c514754d3aa2b21a927c8c526e6c22c22c5a
maen <maenkh@yahoo.com>
2023-08-28 14:42:15 +0300

    new version bubble sort

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,16 +1,18 @@
-
-
 function bubbleSort(arr) {
-  for (let j = 0; j < arr.length; j++) {
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > arr[i + 1]) {
-        let temp = arr[i + 1];
-        arr[i + 1] = arr[i];
-        arr[i] = temp;
+  const swap = (arr, j) => {
+    let temp = arr[j];
+    arr[j] = arr[j + 1];
+    arr[j + 1] = temp;
+  };
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j);
       }
     }
   }
+
   return arr;
 }
 
-console.log(bubbleSort([7,6, 3, 1, 2, 5, 4]));
+console.log(bubbleSort([40, 7, 6, 3, 1, 2, 5, 4]));
