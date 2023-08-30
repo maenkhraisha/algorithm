67f644d193005e034fe994b4dc2e0063640424ac
maen <maenkh@yahoo.com>
2023-08-30 15:46:20 +0300

    fix

--- sort_insertion.js
+++ sort_insertion.js
@@ -2,19 +2,23 @@ function insertion(arr) {
   // loop from the second element and
   // find where is this element fit in the previouse portion
 
+
   for (let i = 1; i < arr.length; i++) {
-    for(let j = i-1; j >= 0; j--){
-        console.log(arr[i],arr[j]);
-        let temp = arr[j];
-        if(arr[i] > arr[j]){
-            arr[i+1]= temp;
-        }else if(arr[i] < arr[j]){
-            arr[i] = arr[i+1]
-        }
+    let temp = arr[i];
+    let j;
+    for( j = i - 1; j >= 0 && arr[j] > temp ; j--){    
+            arr[j+1]= arr[j]    
     }
+    arr[j+1] = temp;
+    console.log("---------------");
   }
 
   return arr;
 }
 
-console.log(insertion([7, 3, 2, 5, 4, 6, 1]));
+console.log(insertion([7, 3, 2, 5, 4, 6, 1,1]));
+
+// [ 2 , 2 , 3  , 5 , 4 , 6 , 7 ]
+//                i
+//   j
+//temp = 1
