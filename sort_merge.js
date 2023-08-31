1fbbfe3e0699635d6b5b9722a4cd34a9f8d4f5d9
maen <maenkh@yahoo.com>
2023-08-31 12:25:16 +0300

    merge sort

---
+++ sort_merge.js
@@ -0,0 +1,16 @@
+function mergeSort(arr1, arr2) {
+  
+  
+  function merge(arr1, arr2) {
+    let l = arr1.length + arr2.length;
+    let arr = [l];
+
+    for (let i = 0; i < arr1.length || i < arr2.length; i++) {
+      
+    }
+  }
+
+  merge(arr1,arr2);
+}
+
+console.log(mergeSort([2, 3, 4], [6, 5, 7]));
